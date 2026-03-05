36ddbd1b82516a23fcb9d81471c15a90d0d6b320
LathikaMahilan <mlathikamlathika78@gmail.com>
2026-03-05 10:00:37 +0530

    Add files via upload

---
+++ 11. Expense Balance Point.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    int total = 0;
+
+    
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        total += arr[i];
+    }
+
+    int left_sum = 0;
+
+    for(int i = 0; i < n; i++) {
+        int right_sum = total - left_sum - arr[i];
+
+        if(left_sum == right_sum) {
+            printf("%d", i);
+            return 0;
+        }
+
+        left_sum += arr[i];
+    }
+
+    printf("-1");
+
+    return 0;
+}
